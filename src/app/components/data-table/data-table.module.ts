5e6ce417c952360988297f67e108e540521299f5
Suhel Khan <skykazi15@gmail.com>
2021-09-12 16:02:41 +0530

    vertual scroll removed

--- src/app/components/data-table/data-table.module.ts
+++ src/app/components/data-table/data-table.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { StoreModule } from '@ngrx/store';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -16,7 +15,6 @@ import { DataTableComponent } from './data-table.component';
     CommonModule,
     SharedModule,
     StoreModule,
-    ScrollingModule,
     DragDropModule,
     SearchBarModule,
     PaginationModule
